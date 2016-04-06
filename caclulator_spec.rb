59a41668ca078eb177eff01fa4c03ea2dd791487
Jessica Wilson <jessicalostinspace@gmail.com>
2016-04-06 10:08:01 -0700

    added rspec

---
+++ caclulator_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'calculator'
+
+RSpec.describe Calculator do
+
+	begin
+		@calculator = Calculator.new
+	end
+
+  it 'has a method to add two arguments' do
+
+	expect(@calculator.add(2,3)).to eq(5)
+
+  end 
+
+
+end
